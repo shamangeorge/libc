49dd5e9ab57d45e92e9f4dd94e8f3b9b2cfe64f7
shamangeorge <shamangeorge@fruitopology.net>
2016-09-23 21:43:31 -0400

    Add memory allocators and copiers.

--- lib/libc.rb
+++ lib/libc.rb
@@ -1,5 +1,18 @@
 require "c/version"
+require 'ffi'
 
 module Libc
-  # Your code goes here...
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
+
+  # allocators
+  attach_function :malloc, [:size_t], :pointer
+  attach_function :calloc, [:size_t], :pointer
+  attach_function :valloc, [:size_t], :pointer
+  attach_function :realloc, [:pointer, :size_t], :pointer
+  attach_function :free, [:pointer], :void
+
+  # movers
+  attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
+  attach_function :bcopy, [:pointer, :pointer, :size_t], :void
 end
